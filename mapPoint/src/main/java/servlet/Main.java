7ba369ba19dd747620c1d0985a23136f0e818f95
Nakanishi Takeshi <nkns3takeshi@gmail.com>
2024-12-05 16:16:15 +0900

    アプリの説明と作成理由を記述

---
+++ mapPoint/src/main/java/servlet/Main.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/Main")
+
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+	throws ServletException,IOException {		
+	
+	// メイン画面にフォワード
+			RequestDispatcher dispatcher = 
+					request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request,  response);
+	}		
+}
